878e06a65185f5fa58a58c2c4dafdf59435d1bff
agadzik <andrew.gadzik@vercel.com>
2023-10-05 17:47:40 -0400

    build 4

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,12 @@ import Image from "next/image";
 
 export default function Home() {
   const secret = process.env.GADZIK_SECRET;
+  const publicValue = process.env.GADZIK_PUBLIC;
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {secret ?? "no secret defined"}
+      <p>{secret ?? "no secret defined"}</p>
+      <p>{publicValue ?? "no public value defined"}</p>
     </main>
   );
 }
